6502374d16faaaa577588b272f0e180bf196ba55
SharkSharko <melileiemmanuel@gmail.com>
2025-03-13 19:27:34 +0300

    phase-0-intro-to-js-2-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+// **Destructive Functions**
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// **Non-Destructive Functions**
+function appendCat(name) {
+  return [...cats, name]; // Returns a new array with the cat added to the end
+}
+
+function prependCat(name) {
+  return [name, ...cats]; // Returns a new array with the cat added to the beginning
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1); // Returns a new array without the last cat
+}
+
+function removeFirstCat() {
+  return cats.slice(1); // Returns a new array without the first cat
+}
+
+// Export functions for testing
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+};
